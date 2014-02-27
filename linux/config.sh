0f8409873a178f02673b578bdea3d661b7e92eea
charlessoft <charlessoft@qq.com>
2014-02-27 23:25:49 +0800

    check whether exist user jenkins

--- linux/config.sh
+++ linux/config.sh
@@ -1,3 +1,8 @@
-sudo /usr/sbin/useradd -m jenkins -d /home/jenkins 
+if cat /etc/passwd | awk -F  : '{print $1}' | grep jenkins >/dev/null 2>&1
+then
+    echo "user already exist"
+else
+    sudo /usr/sbin/useradd -m jenkins -d /home/jenkins 
+fi
 echo "jenkins:jenkins" | chpasswd
 su - jenkins
