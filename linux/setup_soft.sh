763cf2a4b59d5b0fc20b257a7a31f17ff1c4a82b
charlessoft <charlessoft@qq.com>
2014-03-01 20:11:08 -0800

    setup soft

---
+++ linux/setup_soft.sh
@@ -0,0 +1,14 @@
+cp apache-maven-3.2.1-bin.zip /usr/local/java
+cd /usr/local/java
+unzip apache-maven-3.2.1-bin.zip
+ln -s apache-maven-3.2.1 apache-maven
+
+#echo "export JAVA_HOME=/usr/local/java/jdk1.6.0_38" >> /etc/profile
+#echo "export JAVA_BIN=/usr/local/java/jdk1.6.0_38/bin" >> /etc/profile
+#echo "export M2_HOME=/usr/local/java/apache-maven" >> /etc/profile
+#echo "export PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin" >> /etc/profile
+#echo "export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar" >> /etc/profile
+#echo "export JAVA_HOME JAVA_BIN PATH CLASSPATH" >> /etc/profile
+
+
+
