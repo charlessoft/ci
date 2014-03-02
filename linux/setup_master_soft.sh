e39786a7e287f124137dbca68eb5d9aed73cd670
charlessoft <charlessoft@qq.com>
2014-03-01 20:24:27 -0800

    master 安装 tomcat

--- linux/setup_master_soft.sh
+++ linux/setup_master_soft.sh
@@ -1,8 +1,3 @@
-cp apache-maven-3.2.1-bin.zip /usr/local/java
-cd /usr/local/java
-unzip apache-maven-3.2.1-bin.zip
-ln -s apache-maven-3.2.1 apache-maven
-
 #echo "export JAVA_HOME=/usr/local/java/jdk1.6.0_38" >> /etc/profile
 #echo "export JAVA_BIN=/usr/local/java/jdk1.6.0_38/bin" >> /etc/profile
 #echo "export M2_HOME=/usr/local/java/apache-maven" >> /etc/profile
@@ -11,4 +6,17 @@ ln -s apache-maven-3.2.1 apache-maven
 #echo "export JAVA_HOME JAVA_BIN PATH CLASSPATH" >> /etc/profile
 
 
+#maven
+#cp apache-maven-3.2.1-bin.zip /usr/local/java
+#cd /usr/local/java
+#unzip apache-maven-3.2.1-bin.zip
+#ln -s apache-maven-3.2.1 apache-maven
 
+
+#tomcat
+cp apache-tomcat-7.0.52.tar.gz /usr/local/java
+cd /usr/local/java
+tar zxvf apache-tomcat-7.0.52.tar.gz
+ln -s apache-tomcat-7.0.52 apache-tomcat
+cd apache-tomcat/bin
+#./startup.sh
