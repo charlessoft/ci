f5cdf68e72b5d02c070002f2b408382c00500294
charlessoft <charlessoft@qq.com>
2014-05-22 00:15:09 +0800

    modify create users

--- linux/UserMgr/CreateUser.sh
+++ linux/UserMgr/CreateUser.sh
@@ -1,2 +1,11 @@
-newusers userlist.txt
-chpasswd < userpasswd.txt
+#!/bin/bash
+path=$(dirname $0)
+
+echo path=$(pwd)/$path
+#当前目录保存到变量D中
+#D=$(pwd)
+#根据当前目录得到上级目录
+#P=$(dirname $D)
+
+newusers $path/userlist.txt
+chpasswd < $path/userpasswd.txt
