38d527432be7af066ddcff287423d483ddf88402
charlessoft <charlessoft@qq.com>
2014-05-22 00:19:10 +0800

    modify env.sh

---
+++ linux/env.sh
@@ -0,0 +1,6 @@
+echo export JAVA_HOME=/usr/local/jdk >> /etc/profile
+echo export JAVA_BIN=$JAVA_HOME/bin >> /etc/profile
+echo export M2_HOME=/usr/local/apache-maven >> /etc/profile
+echo export PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin >> /etc/profile
+echo export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar >> /etc/profile
+echo export JAVA_HOME JAVA_BIN PATH CLASSPATH >> /etc/profile
