29bc7782139bee9f14896ccd0f978de7b2f93e52
charlessoft <charlessoft@qq.com>
2014-08-01 12:49:28 +0800

    add svn

--- linux/env.sh
+++ linux/env.sh
@@ -13,7 +13,8 @@ echo CLASSPATH=$CLASSPATH
 echo export JAVA_HOME=$JAVA_HOME >> /etc/profile
 echo export JAVA_BIN=$JAVA_HOME/bin >> /etc/profile
 echo export M2_HOME=/usr/local/apache-maven >> /etc/profile
-echo export PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin >> /etc/profile
+echo export SVN_HOME=/usr/local/svn >> /etc/profile
+echo export PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin:$SVN_HOME/bin >> /etc/profile
 echo export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar >> /etc/profile
 echo export JAVA_HOME JAVA_BIN PATH CLASSPATH >> /etc/profile
 su - root -c 'source /etc/profile'
