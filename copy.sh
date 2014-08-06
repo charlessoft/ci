0c919713992c4575d07787e4f966495502b6dbce
charlessoft <charlessoft@qq.com>
2014-08-06 14:25:32 +0800

    fix apache install path

--- copy.sh
+++ copy.sh
@@ -1,3 +1,5 @@
-scp -r ../ci/ charles@10.211.55.6:/home/charles/
+#scp -r ../ci/ charles@10.211.55.3:/home/charles/
+#scp -r ../ci/ charles@10.211.55.6:/home/charles/
+scp -r ../ci/ charles@10.211.55.5:/home/charles/
 #scp charles@10.211.55.6:/home/charles/ci/linux/Makefile ./Makefile_bak
-
+#scp -r ../ci/ root@10.142.55.227:/home/root/
