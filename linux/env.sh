c1a812658dae50af7a5fd85459d98e22437331c8
charlessoft <charlessoft@qq.com>
2014-08-09 14:58:08 +0800

    add sonar demo

--- linux/env.sh
+++ linux/env.sh
@@ -4,7 +4,8 @@ JAVA_BIN=$JAVA_HOME/bin
 M2_HOME=/usr/local/apache-maven 
 SVN_HOME=/usr/local/svn
 JMETER_HOME=/usr/local/jmeter
-PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin:$SVN_HOME/bin:$JMETER_HOME/bin
+SONAR_RUNNER_HOME=/usr/local/sonar_runner
+PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin:$SVN_HOME/bin:$JMETER_HOME/bin:$SONAR_RUNNER_HOME/bin
 CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar 
 echo JAVA_HOME=$JAVA_HOME 
 echo JAVA_BIN=$JAVA_BIN 
@@ -17,7 +18,9 @@ echo export JAVA_BIN=$JAVA_HOME/bin >> /etc/profile
 echo export M2_HOME=/usr/local/apache-maven >> /etc/profile
 echo export SVN_HOME=/usr/local/svn >> /etc/profile
 echo export JMETER_HOME=/usr/local/jmeter >> /etc/profile
-echo export PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin:$SVN_HOME/bin:$JMETER_HOME/bin >> /etc/profile
+echo export SONAR_RUNNER_HOME=$SONAR_RUNNER_HOME >> /etc/profile 
+echo export PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin:$SVN_HOME/bin:$JMETER_HOME/bin:$SONAR_HOME/bin:$SONAR_RUNNER_HOME/bin >> /etc/profile
+
 echo export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar >> /etc/profile
 echo export JAVA_HOME JAVA_BIN PATH CLASSPATH >> /etc/profile
 su - root -c 'source /etc/profile'
